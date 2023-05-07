d194a9b357650db6d38cc4719f65c5804156fb37
José Matheus <XxFULLDLCxX@outlook.com>
2023-05-07 06:38:51 -0300

    Arrumando o Sidebar que não ficava fixo, no edge

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -5,16 +5,18 @@ export default function SideBar() {
     const user = { image: { src: "./assets/img/catanacomics.svg", alt: "imagem de perfil" }, name: "catanacomics" };
     return (
         < div className="sidebar" >
-            < User image={user.image} name={user.name} />
-            < Suggestions />
+            <div className="sidebar-content">
+                < User image={user.image} name={user.name} />
+                < Suggestions />
 
-            <div className="links">
-                Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
-                Hashtags • Idioma
-            </div>
+                <div className="links">
+                    Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
+                    Hashtags • Idioma
+                </div>
 
-            <div className="copyright">
-                © 2021 INSTAGRAM DO FACEBOOK
+                <div className="copyright">
+                    © 2021 INSTAGRAM DO FACEBOOK
+                </div>
             </div>
         </div >
     );
